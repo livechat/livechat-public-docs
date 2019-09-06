a9a80e05e10184d21fc4b7de32e91d5b86df299f
Filip Jaskólski <f.jaskolski@livechatinc.com>
2019-09-06 17:51:09 +0200

    added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,6 +60,13 @@ module.exports = {
         display: "minimal-ui"
         // icon: "src/images/gatsby-icon.png" // This path is relative to the root of the site.
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`source sans pro\:400,500,600,700`],
+        display: "swap"
+      }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
