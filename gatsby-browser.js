dcae3ed83fd8effff5da767fd42e40b1736dad8a
Jakub Sikora <j.sikora@livechatinc.com>
2020-02-19 11:03:56 +0100

    move offset to constants, tweak margins

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import "docsearch.js/dist/cdn/docsearch.min.css";
 import docsearch from "docsearch.js/dist/cdn/docsearch.min.js";
 import { setupAmplitude } from "./src/utils";
+import { SCROLL_OFFSET } from "./src/constant";
 
 window.docsearch = docsearch;
 
@@ -8,7 +9,7 @@ if (typeof window !== "undefined") {
   require("smooth-scroll")('a[href*="#"]', {
     speed: 160,
     speedAsDuration: true,
-    offset: 140
+    offset: SCROLL_OFFSET
   });
   setupAmplitude();
 }
