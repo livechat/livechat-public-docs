cd55665e67850ffb1a4dcb6f250a9ad0c1fbaff5
Aleksandra Kacperczyk <a.kacperczyk@livechatinc.com>
2020-03-13 10:57:29 +0100

    Fixed openChatWindow upon link click

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,7 +6,8 @@ import { SCROLL_OFFSET } from "./src/constant";
 window.docsearch = docsearch;
 
 if (typeof window !== "undefined") {
-  require("smooth-scroll")('a[href*="#"]', {
+  // use #open-chat when adding openChatWindow to links, otherwise it'll scroll up upon click
+  require("smooth-scroll")('a[href*="#"]:not([href="#open-chat"])', {
     speed: 160,
     speedAsDuration: true,
     offset: SCROLL_OFFSET
