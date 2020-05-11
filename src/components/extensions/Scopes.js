0477de593f8605765becfd94416009471d953fbc
Jakub Sikora <j.sikora@livechatinc.com>
2020-05-11 13:25:57 +0200

    (DPS-1799) Scopes component #642

---
+++ src/components/extensions/Scopes.js
@@ -0,0 +1,71 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { SCOPES } from "../../constant";
+
+const HeadingLink = styled.a`
+  color: inherit;
+  text-decoration: none;
+  position: relative;
+  &:hover {
+    color: inherit;
+    text-decoration: none;
+    &:before {
+      content: "#";
+      position: absolute;
+      font-weight: 300;
+      left: -1em;
+      top: 1px;
+      opacity: 0.3;
+    }
+  }
+`;
+
+const TableWrapper = styled.div`
+  width: 100%;
+  overflow-x: auto;
+`;
+
+const Scopes = ({ title, scopes }) => {
+  const allScopes = scopes.split(",");
+  const formattedScopes = allScopes.map((scope) => {
+    const newScope = SCOPES.find((item) => item.id === scope);
+
+    return newScope;
+  });
+
+  const titleId = title.toLowerCase().replace(" ", "-");
+
+  return (
+    <>
+      <h4 id={titleId} className="heading">
+        <HeadingLink href={`#${titleId}`}>{title}</HeadingLink>
+      </h4>
+
+      <TableWrapper>
+        <table>
+          <thead>
+            <tr>
+              <th>Scope</th>
+              <th>role</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {formattedScopes.map((scope, index) => (
+              <tr key={`scope-${titleId}-${index}`}>
+                <td>
+                  <code className="language-text">{scope.id}</code>
+                </td>
+                <td>{scope.role}</td>
+                <td>{scope.description}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </TableWrapper>
+    </>
+  );
+};
+
+export default Scopes;
