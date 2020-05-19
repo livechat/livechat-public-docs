9ecc28f1957d4617180793c6134c1ef8559f854a
Jakub Sikora <j.sikora@livechatinc.com>
2020-05-19 15:53:20 +0200

    new plugin with link parser, use in monetization

---
+++ plugins/gatsby-remark-placeholders/parsers/index.js
@@ -0,0 +1,5 @@
+const link = require("./link");
+
+module.exports = {
+  link,
+};
---
+++ plugins/gatsby-remark-placeholders/placeholders.js
@@ -0,0 +1,16 @@
+const placeholders = [
+  {
+    from: "{{DEVELOPER_CONSOLE_URL}}",
+    to: "https://developers.livechat.com/console/",
+    type: "link",
+    label: "Developer Console",
+  },
+  {
+    from: "{{LIVECHAT_HOME_URL}}",
+    to: "https://www.livechat.com",
+    type: "link",
+    label: "LiveChat",
+  },
+];
+
+module.exports = placeholders;
