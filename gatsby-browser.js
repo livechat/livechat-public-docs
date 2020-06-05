ff44924717d452d9ea13e3f6abe8728910c04660
Aleksandra Kacperczyk <a.kacperczyk@livechatinc.com>
2020-06-05 11:09:31 +0200

    Test only json and js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,6 +7,7 @@ window.docsearch = docsearch;
 
 if (typeof window !== "undefined") {
   // use #open-chat when adding openChatWindow to links, otherwise it'll scroll up upon click
+  //comment
   require("smooth-scroll")('a[href*="#"]:not([href="#open-chat"])', {
     speed: 160,
     speedAsDuration: true,
