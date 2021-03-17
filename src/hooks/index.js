42c3fb7cace8927f5f8c3f7e9f95c91093901f62
Jakub Sikora <j.sikora@livechatinc.com>
2021-03-17 09:31:02 +0100

    wip

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,19 +1,19 @@
-import useAllArticlesInCategory from "./useAllArticlesInCategory";
-import useCategoryMeta from "./useCategoryMeta";
-import useAllCategoriesMeta from "./useAllCategoriesMeta";
-import useAllArticlesGroupedByCategory from "./useAllArticlesGroupedByCategory";
-import useAllArticlesGroupedBySubcategory from "./useAllArticlesGroupedBySubcategory";
-import useScrollSpy from "./useScrollSpy";
+// import useAllArticlesInCategory from "./useAllArticlesInCategory";
+// import useCategoryMeta from "./useCategoryMeta";
+// import useAllCategoriesMeta from "./useAllCategoriesMeta";
+// import useAllArticlesGroupedByCategory from "./useAllArticlesGroupedByCategory";
+// import useAllArticlesGroupedBySubcategory from "./useAllArticlesGroupedBySubcategory";
+// import useScrollSpy from "./useScrollSpy";
 import useLocalStorage from "./useLocalStorage";
 import useRating from "./useRating";
 
 export {
-  useAllArticlesInCategory,
-  useCategoryMeta,
-  useAllCategoriesMeta,
-  useAllArticlesGroupedByCategory,
-  useAllArticlesGroupedBySubcategory,
-  useScrollSpy,
+  // useAllArticlesInCategory,
+  // useCategoryMeta,
+  // useAllCategoriesMeta,
+  // useAllArticlesGroupedByCategory,
+  // useAllArticlesGroupedBySubcategory,
+  // useScrollSpy,
   useLocalStorage,
-  useRating
+  useRating,
 };
