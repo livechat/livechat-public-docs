45b69f41ca4529e178a75b57e3e69f7b8673cf89
Michał Treter <m.treter@livechatinc.com>
2021-03-23 10:48:40 +0100

    API-8319: Document new event form (#855)

--- payloads/messaging/v3.3/agent-chat-api/index.js
+++ payloads/messaging/v3.3/agent-chat-api/index.js
@@ -1,17 +1,19 @@
-export { default as custom } from "./events/custom.json"
-export { default as file } from "./events/file.json"
-export { default as filledForm } from "./events/filledForm.json"
-export { default as message } from "./events/message.json"
-export { default as richMessage } from "./events/richMessage.json"
-export { default as systemMessage } from "./events/systemMessage.json"
+export { default as custom } from "./events/custom.json";
+export { default as file } from "./events/file.json";
+export { default as filledForm } from "./events/filledForm.json";
+export { default as form } from "./events/form.json";
+export { default as message } from "./events/message.json";
+export { default as richMessage } from "./events/richMessage.json";
+export { default as systemMessage } from "./events/systemMessage.json";
 
-export { default as agent } from "./users/agent.json"
-export { default as myProfile } from "./users/myProfile.json"
-export { default as customer } from "./users/customer.json"
+export { default as agent } from "./users/agent.json";
+export { default as myProfile } from "./users/myProfile.json";
+export { default as customer } from "./users/customer.json";
 
-export { default as access } from "./other-structures/access.json"
-export { default as chat } from "./other-structures/chat.json"
-export { default as chatSummary } from "./other-structures/chatSummary.json"
-export { default as formFields } from "./other-structures/formFields.json"
-export { default as properties } from "./other-structures/properties.json"
-export { default as thread } from "./other-structures/thread.json"
+export { default as access } from "./other-structures/access.json";
+export { default as chat } from "./other-structures/chat.json";
+export { default as chatSummary } from "./other-structures/chatSummary.json";
+export { default as formFields } from "./other-structures/formFields.json";
+export { default as filledFormFields } from "./other-structures/filledFormFields.json";
+export { default as properties } from "./other-structures/properties.json";
+export { default as thread } from "./other-structures/thread.json";
