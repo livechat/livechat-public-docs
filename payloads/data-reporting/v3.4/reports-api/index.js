8a18a6bed831ddad771a3e4c1b89095d345e4044
Aleksandra Kacperczyk <a.kacperczyk@livechatinc.com>
2021-04-07 13:11:37 +0200

    Open v3.4

---
+++ payloads/data-reporting/v3.4/reports-api/index.js
@@ -0,0 +1,3 @@
+export { default as agentsChattingDuration } from "./responses/chats/agentsChattingDuration.json";
+export { default as tags } from "./responses/chats/tags.json";
+export { default as totalChats } from "./responses/chats/totalChats.json";
