94bf8e16fef5dbae236e0a59e00fb93d371857de
Jan Owsiany <j.owsiany@livechatinc.com>
2021-04-26 09:17:13 +0200

    API-8669 Document reports-api protocol improvements (#891)

--- payloads/data-reporting/v3.4/reports-api/index.js
+++ payloads/data-reporting/v3.4/reports-api/index.js
@@ -1,3 +1,4 @@
 export { default as agentsChattingDuration } from "./responses/chats/agentsChattingDuration.json";
 export { default as tags } from "./responses/chats/tags.json";
 export { default as totalChats } from "./responses/chats/totalChats.json";
+export { default as filters } from "./filters.json";
