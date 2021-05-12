c817c2f94133aa13bf4aeb0fbc754b4b0998b6d6
arturfracala <arturfracala@gmail.com>
2021-05-12 09:02:41 +0200

    (DPS-2739) Migrate SideNav component  (#899)

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,19 +1,15 @@
-// import useAllArticlesInCategory from "./useAllArticlesInCategory";
-// import useCategoryMeta from "./useCategoryMeta";
-// import useAllCategoriesMeta from "./useAllCategoriesMeta";
-// import useAllArticlesGroupedByCategory from "./useAllArticlesGroupedByCategory";
-// import useAllArticlesGroupedBySubcategory from "./useAllArticlesGroupedBySubcategory";
-// import useScrollSpy from "./useScrollSpy";
+import useArticlesInCategory from "./useArticlesInCategory";
+import useCategoryMeta from "./useCategoryMeta";
+import useAllCategoriesMeta from "./useAllCategoriesMeta";
+import useScrollSpy from "./useScrollSpy";
 import useLocalStorage from "./useLocalStorage";
 import useRating from "./useRating";
 
 export {
-  // useAllArticlesInCategory,
-  // useCategoryMeta,
-  // useAllCategoriesMeta,
-  // useAllArticlesGroupedByCategory,
-  // useAllArticlesGroupedBySubcategory,
-  // useScrollSpy,
+  useArticlesInCategory,
+  useCategoryMeta,
+  useAllCategoriesMeta,
+  useScrollSpy,
   useLocalStorage,
   useRating,
 };
