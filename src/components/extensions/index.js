8382ad1cb333032b941f19875c572452a7d274f8
Jakub Sikora <j.sikora@livechatinc.com>
2021-05-19 15:25:51 +0200

    move plugin, fix section banner

--- src/components/extensions/index.js
+++ src/components/extensions/index.js
@@ -2,6 +2,7 @@ import Headings from "./Headings";
 import * as CodeBlocks from "./CodeBlocks";
 import Scopes from "./Scopes";
 import Errors from "./Errors";
+import Placeholder from "./Placeholder";
 // import Redoc from "./Redoc";
 
-export { Headings, CodeBlocks, Scopes, Errors /*, Redoc*/ };
+export { Headings, CodeBlocks, Scopes, Errors, Placeholder /*, Redoc*/ };
