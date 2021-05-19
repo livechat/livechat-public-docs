853c2e391aaf89707c39bf316945b92398afa7e1
Artur Fracala <arturfracala@gmail.com>
2021-05-19 13:36:11 +0200

    migrated GA plugin

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 import "docsearch.js/dist/cdn/docsearch.min.css";
 import "@livechat/design-system/dist/design-system.css";
 import "normalize.css";
@@ -8,6 +10,12 @@ import "../components/core/algolia.css";
 import "../components/core/redoc.css";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId:
+        process.env.NODE_ENV === "production" ? "GTM-M58RLCQ" : "GTM-5DVQQC",
+    });
+  }, []);
   return <Component {...pageProps} />;
 }
 
