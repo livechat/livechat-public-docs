73bb7f97b3c0b976d282521de7c6957a6a221e8b
Artur Fracala <arturfracala@gmail.com>
2021-05-25 12:48:35 +0200

    removed remnants of previous commits

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -5,6 +5,3 @@ export const VersionProvider = VersionContext.Provider;
 
 export const RatingContext = createContext(null);
 export const RatingProvider = RatingContext.Provider;
-
-export const UniqueIDsContext = createContext(null);
-export const UniqueIDsProvider = UniqueIDsContext.Provider;
