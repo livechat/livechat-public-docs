df7c8c970ffd9059b5502e1a724023a127c68924
Jakub Sikora <j.sikora@livechatinc.com>
2021-05-28 15:21:55 +0200

    redoc working, fix styles

--- next.config.js
+++ next.config.js
@@ -35,23 +35,3 @@ module.exports = withPlugins(
   ],
   nextConfig
 );
-
-// module.exports = withMdxEnhanced({
-//   layoutPath: "src/components/Page",
-//   defaultLayout: true,
-//   fileExtensions: ["mdx"],
-//   remarkPlugins: [],
-//   rehypePlugins: [require("rehype-slug"), require("rehype-autolink-headings")],
-//   usesSrc: false,
-//   extendFrontMatter: {
-//     process: (mdxContent, frontMatter) => {
-//       return {
-//         timeToRead: readingTime(mdxContent),
-//       };
-//     },
-//     phase: "both",
-//   },
-//   reExportDataFetching: false,
-// })({
-//   target: "serverless",
-// });
