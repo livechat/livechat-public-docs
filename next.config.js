a65f88dc8eba51722983dc68f21af8a9722f73de
Jakub Sikora <j.sikora@livechatinc.com>
2021-06-30 15:58:33 +0200

    fixing images, base path

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const readingTime = require("reading-time");
 const nextConfig = {
   target: "serverless",
   trailingSlash: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
 };
 
 module.exports = withPlugins(
