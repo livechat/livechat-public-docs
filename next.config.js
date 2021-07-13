7469cf4843e066ae8f6d142be529b1f22ae7fd93
Jakub Sikora <j.sikora@livechatinc.com>
2021-07-13 15:24:21 +0200

    fixes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 const withPlugins = require("next-compose-plugins");
 const withMdxEnhanced = require("next-mdx-enhanced");
 const withYaml = require("next-plugin-yaml");
-const readingTime = require("reading-time");
-// const withImages = require("next-images");
 
 const nextConfig = {
   target: "serverless",
@@ -24,23 +22,11 @@ module.exports = withPlugins(
           require("@mapbox/rehype-prism"),
         ],
         usesSrc: false,
-        extendFrontMatter: {
-          process: (mdxContent, frontMatter) => {
-            const time = readingTime(mdxContent);
-            if (time.minutes < 0.5) {
-              time.minutes = 1;
-            }
-            return {
-              timeToRead: time,
-            };
-          },
-          phase: "both",
-        },
+        extendFrontMatter: {},
         reExportDataFetching: false,
       }),
     ],
     [withYaml],
-    // [withImages],
   ],
   nextConfig
 );
