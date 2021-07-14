c5bdc560d3550338d12e0d61aa39668bb5c7cd70
Jakub Sikora <j.sikora@livechatinc.com>
2021-07-14 10:37:13 +0200

    fixes dirs

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withYaml = require("next-plugin-yaml");
 const nextConfig = {
   target: "serverless",
   basePath: "/docs",
-  distDir: `dist/docs`,
+  // distDir: `dist/docs`,
 };
 
 module.exports = withPlugins(
