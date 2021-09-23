d43982ae75b8bbc7de265965b3a927ef27ef377c
Grzegorz Gajda <grz.gajda@outlook.com>
2021-09-23 12:41:35 +0200

    API-9700: docs for chats/duration report

--- payloads/data-reporting/v3.4/reports-api/index.js
+++ payloads/data-reporting/v3.4/reports-api/index.js
@@ -1,4 +1,4 @@
-export { default as agentsChattingDuration } from "./responses/chats/agentsChattingDuration.json";
+export { default as duration } from "./responses/chats/duration.json";
 export { default as tags } from "./responses/chats/tags.json";
 export { default as totalChats } from "./responses/chats/totalChats.json";
 export { default as filters } from "./filters.json";
