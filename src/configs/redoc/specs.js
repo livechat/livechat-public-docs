8a20e6f35e5a73c0fe7a6794df8a5d762ca581d0
Kacper Frąckowski <45572849+kacperf531@users.noreply.github.com>
2021-10-05 13:10:22 +0200

    API-9574: Add description of new endpoint for disabled chat between g… (#1030)

--- src/configs/redoc/specs.js
+++ src/configs/redoc/specs.js
@@ -1,9 +1,12 @@
 const globalAccountsApiSpec = require("./global-accounts-api/spec.yml");
 const customerAccountsApiSpec = require("./customer-accounts-api/spec.yml");
+const customerAccountsApiSpecv2 = require("./customer-accounts-api/specv2.yml");
+
 
 const specs = {
   "global-accounts-api": globalAccountsApiSpec,
   "customer-accounts-api": customerAccountsApiSpec,
+  "customer-accounts-api-v2": customerAccountsApiSpecv2
 };
 
 module.exports = specs;
