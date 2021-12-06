3d7798bee23bfecf851e951b04480e192c140bae
Artur Fracala <arturfracala@gmail.com>
2021-12-06 17:56:18 +0100

    CR fixes

--- src/utils/index.js
+++ src/utils/index.js
@@ -119,8 +119,9 @@ export const getStableVersion = (category) => {
   switch (category) {
     case "extending-chat-widget":
       return VERSIONS_GROUPS["chat-widget"].STABLE_VERSION;
+    case "data-reporting":
+      return VERSIONS_GROUPS["data-reporting"].STABLE_VERSION;
     default:
       return VERSIONS_GROUPS.DEFAULT.STABLE_VERSION;
-      break;
   }
 };
