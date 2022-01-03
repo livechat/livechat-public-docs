02318761212022d2eb14c7c649fb30a6fe8a4ebc
Aleksandra Kacperczyk <a.kacperczyk@livechatinc.com>
2022-01-03 15:52:06 +0100

    Make v3.4 stable and add v3.5

---
+++ payloads/management/v3.5/configuration-api/index.js
@@ -0,0 +1,24 @@
+export { default as createAgent } from "./responses/agents/createAgent.json";
+export { default as getAgent } from "./responses/agents/getAgent.json";
+export { default as listAgents } from "./responses/agents/listAgents.json"
+
+export { default as createBot } from "./responses/bots/createBot.json"
+export { default as getBot } from "./responses/bots/getBot.json"
+export { default as listBots } from "./responses/bots/listBots.json"
+
+export { default as createGroup } from "./responses/groups/createGroup.json"
+export { default as getGroup } from "./responses/groups/getGroup.json"
+export { default as listGroups } from "./responses/groups/listGroups.json"
+
+export { default as listGroupProperties } from "./responses/properties/listGroupProperties.json"
+export { default as listLicenseProperties } from "./responses/properties/listLicenseProperties.json"
+export { default as listProperties } from "./responses/properties/listProperties.json"
+
+export { default as registerWebhook } from "./responses/webhooks/registerWebhook.json"
+export { default as listWebhooks } from "./responses/webhooks/listWebhooks.json"
+export { default as listWebhookNames } from "./responses/webhooks/listWebhookNames.json"
+
+export { default as getWebhooksState } from "./responses/webhooks/getWebhooksState.json"
+
+export { default as getOrganizationID } from "./responses/other/getOrganizationID.json"
+export { default as getLicenseID } from "./responses/other/getLicenseID.json"
