f91bc92af8b729c27ad95878ffa37792c4ca5648
Grzegorz Gajda <grz.gajda@outlook.com>
2022-04-03 16:57:07 +0200

    API-10179: remove unused scopes from list

--- src/constant/index.js
+++ src/constant/index.js
@@ -345,14 +345,14 @@ const SCOPES = [
   },
   {
     id: "properties.group.value--my.rw",
-    description: "Read/write permission for managing groups' properties (only my namespace)",
+    description: "Read/write permission for managing groups properties (only my namespace)",
     role: "administrator",
     role_type: "secondary",
     product: "Livechat"
   },
   {
     id: "properties.group.value--all.rw",
-    description: "Read/write permission for managing groups' properties (in all namespaces but with access only to their public properties)",
+    description: "Read/write permission for managing groups properties (in all namespaces but with access only to their public properties)",
     role: "administrator",
     role_type: "secondary",
     product: "Livechat"
