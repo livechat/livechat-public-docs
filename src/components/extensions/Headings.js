0053e35f25a97c11ef6316212093079c7e503a0f
Artur Fracala <arturfracala@gmail.com>
2022-05-09 13:47:18 +0200

    wip

--- src/components/extensions/Headings.js
+++ src/components/extensions/Headings.js
@@ -1,5 +1,5 @@
 import React from "react";
-/** @jsx jsx */
+/** @jsxImportSource @emotion/react */
 import { jsx, css } from "@emotion/core";
 import styled from "@emotion/styled";
 import Link from "next/link";
---
+++ src/pages/[...slug].js
@@ -0,0 +1,91 @@
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
+import path from "path";
+import fs from "fs";
+import matter from "gray-matter";
+import glob from "fast-glob";
+import PageLayout from "../components/Page";
+import {
+  Headings,
+  CodeBlocks,
+  Scopes,
+  Errors,
+  Placeholder,
+} from "../components/extensions";
+
+const components = {
+  ...CodeBlocks,
+  ...Headings,
+  Scopes,
+  Errors,
+  Placeholder,
+};
+
+export default function Page({ frontMatter, source }) {
+  return (
+    <PageLayout frontMatter={frontMatter}>
+      <MDXRemote {...source} components={components} />
+    </PageLayout>
+  );
+}
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const postSlug = slug.join("/");
+
+  const POSTS_PATH = path.join(
+    process.cwd(),
+    "src/content/" + postSlug + "/index.mdx"
+  );
+  const source = fs.readFileSync(POSTS_PATH);
+
+  const { content, data } = matter(source);
+
+  const mdxSource = await serialize(content, {
+    // Optionally pass remark/rehype plugins
+    mdxOptions: {
+      remarkPlugins: [],
+      rehypePlugins: [
+        require("rehype-slug"),
+        require("rehype-autolink-headings"),
+        require("@mapbox/rehype-prism"),
+      ],
+    },
+    scope: data,
+  });
+
+  return {
+    props: {
+      source: mdxSource,
+      frontMatter: data,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const getFilePaths = (source) => {
+    const contentGlob = `${source}/**/*.mdx`;
+    const files = glob.sync(contentGlob);
+
+    if (!files.length) return [];
+
+    return files;
+  };
+
+  const POSTS_PATH = path.join(process.cwd(), "src/content");
+  const posts = getFilePaths(POSTS_PATH);
+
+  const paths = posts.map((slug) => ({
+    params: {
+      slug: slug
+        .replace(POSTS_PATH, "")
+        .substring(1)
+        .replace("/index.mdx", "")
+        .split("/"),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
