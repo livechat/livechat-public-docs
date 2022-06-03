f1ce7caadef2ae4fb081e225f7794b67a2c3a365
Artur Fracala <arturfracala@gmail.com>
2022-06-03 15:44:18 +0200

    fix version component

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -127,7 +127,7 @@ const Page = ({ frontMatter, children }) => {
         }
       }
 
-      router.push(currentSlug.replace("/docs", ""));
+      router.push("/" + currentSlug.replace("/docs", ""));
     }
   };
 
