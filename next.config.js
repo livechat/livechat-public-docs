331b8f5734980c71b5e9164c7deb2637ab451487
Artur Fracala <arturfracala@gmail.com>
2022-06-17 14:20:40 +0200

    delete slash from basePath in next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withYaml = require("next-plugin-yaml");
 
 module.exports = withYaml({
-  basePath: process.env.CONTEXT === "deploy-preview" ? "/" : "/docs",
+  basePath: process.env.CONTEXT === "deploy-preview" ? "" : "/docs",
   target: "serverless",
   trailingSlash: true,
 });
