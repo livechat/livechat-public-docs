8ced174cbdce52e7c3ed66a9c0d8994770b41358
Artur Fracala <arturfracala@gmail.com>
2023-03-17 17:11:38 +0100

    fix typos

---
+++ src/constant/cards.js
@@ -0,0 +1,119 @@
+export const cards = [
+  {
+    title: "Messaging",
+    link: "/messaging",
+    copy: "Event-based messaging for sync and async communication.",
+    image: "/icons/messaging.png",
+  },
+  {
+    title: "Management",
+    link: "/management",
+    copy: "Configure resources. Set up webhooks and properties.",
+    image: "/icons/management.png",
+  },
+  {
+    title: "Data & Reporting",
+    link: "/data-reporting",
+    copy: "Extract data to build custom reports and dashboards.",
+    image: "/icons/data-and-raporting.png",
+  },
+  {
+    title: "Agent App SDK",
+    link: "/extending-agent-app/products-sdk/agent-app-sdk",
+    copy: "Integrate LiveChat with external software or a custom app.",
+    image: "/icons/agent-app-sdk.png",
+  },
+  {
+    title: "Chat Widget",
+    link: "/extending-chat-widget",
+    copy: "Monitor and interact with visitors via rich messages and apps.",
+    image: "/icons/chat-widget.png",
+  },
+  {
+    title: "HelpDesk API",
+    link: "/getting-started/helpdesk-apps/helpdesk-api",
+    copy: "Build integration for our ticketing system, HelpDesk.",
+    image: "/icons/help-desk-api.png",
+  },
+  {
+    title: "Authorization",
+    link: "/authorization",
+    copy: "Authenticate and authorize users with OAuth flows. Manage scopes.",
+    image: "/icons/authorization.png",
+  },
+  {
+    title: "Monetization",
+    link: "/monetization",
+    copy: "Seamless payment configuration right within the Platform.",
+    image: "/icons/monetization.png",
+  },
+  {
+    title: "Developer Console Tools",
+    link: "/console/tools/personal-access-tokens",
+    copy: "Configure your app with a GUI instead of calling the API.",
+    image: "/icons/developer-console-tools.png",
+  },
+  {
+    title: "Text API",
+    link: "/",
+    copy: "Build products powered by advanced text operations.",
+    image: "/icons/text-api.png",
+  },
+];
+
+export const devPaths = [
+  {
+    title: "I’m a beginner",
+    subtitle: "Not sure where to start? Try the articles below:",
+    image: "/icons/beginner-path.png",
+    links: [
+      {
+        link: "/getting-started",
+        name: "Platform Overview",
+      },
+      {
+        link: "/getting-started/app-guides",
+        name: "App Guides",
+      },
+      {
+        link: "/getting-started/livechat-apps",
+        name: "Building LiveChat apps",
+      },
+      {
+        link: "/getting-started/helpdesk-apps",
+        name: "Building HelpDesk apps",
+      },
+      {
+        link: "/monetization/how-to-monetize-apps",
+        name: "How to monetize apps",
+      },
+    ],
+  },
+  {
+    title: "I’m advanced",
+    subtitle: "Already fluent in tech talk? Give those a try:",
+    image: "/icons/advanced-path.png",
+    links: [
+      {
+        link: "/messaging",
+        name: "Messaging Overview",
+      },
+      {
+        link: "/extending-agent-app/products-sdk",
+        name: "Products SDK",
+      },
+      {
+        link: "/extending-chat-widget/javascript-api",
+        name: "Chat Widget JS API",
+      },
+      {
+        link: "/management/webhooks",
+        name: "Webhooks",
+      },
+      {
+        link: "/data-reporting/reports-api",
+        name: "Reports API",
+      },
+    ],
+  },
+];
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ import Header from "../components/core/header";
 import Footer from "../components/core/Footer/Footer";
 import Card from "components/core/Card/Card";
 import DeveloperPath from "components/core/DeveloperPath/DeveloperPath";
+import { cards, devPaths } from "constant/cards";
 
 const wrapperCss = css`
   max-width: 960px;
@@ -41,126 +42,6 @@ const pathsWrapperCss = css`
   margin-top: 32px;
 `;
 
-const cards = [
-  {
-    title: "Messaging",
-    link: "/messaging",
-    copy: "Event-based messaging for sync and async communication.",
-    image: "/docs/icons/messaging.png",
-  },
-  {
-    title: "Managment",
-    link: "/managment",
-    copy: "Configure resources. Set up webhooks and properties.",
-    image: "/docs/icons/managment.png",
-  },
-  {
-    title: "Data & Reporting",
-    link: "/messaging",
-    copy: "Extract data to build custom reports and dashboards.",
-    image: "/docs/icons/data-and-raporting.png",
-  },
-  {
-    title: "Agent App SDK",
-    link: "/messaging",
-    copy: "Integrate LiveChat with external software or a custom app.",
-    image: "/docs/icons/agent-app-sdk.png",
-  },
-  {
-    title: "Chat Widget",
-    link: "/messaging",
-    copy: "Monitor and interact with visitors via rich messages and apps.",
-    image: "/docs/icons/chat-widget.png",
-  },
-  {
-    title: "HelpDesk API",
-    link: "/managment",
-    copy: "Build integration for our ticketing system, HelpDesk.",
-    image: "/docs/icons/help-desk-api.png",
-  },
-  {
-    title: "Authorization",
-    link: "/messaging",
-    copy: "Authenticate and authorize users with OAuth flows. Manage scopes.",
-    image: "/docs/icons/authorization.png",
-  },
-  {
-    title: "Monetization",
-    link: "/messaging",
-    copy: "Seamless payment configuration right within the Platform.",
-    image: "/docs/icons/monetization.png",
-  },
-  {
-    title: "Developer Console Tools",
-    link: "/messaging",
-    copy: "Configure your app with a GUI instead of calling the API.",
-    image: "/docs/icons/developer-console-tools.png",
-  },
-  {
-    title: "Text API",
-    link: "/messaging",
-    copy: "Build products powered by advanced text operations.",
-    image: "/docs/icons/text-api.png",
-  },
-];
-
-const devPaths = [
-  {
-    title: "I’m a beginner",
-    subtitle: "Not sure where to start? Try the articles below:",
-    image: "/docs/icons/beginner-path.png",
-    links: [
-      {
-        link: "",
-        name: "Platform Overview",
-      },
-      {
-        link: "",
-        name: "App Guides",
-      },
-      {
-        link: "",
-        name: "Building LiveChat apps",
-      },
-      {
-        link: "",
-        name: "Building HelpDesk apps",
-      },
-      {
-        link: "",
-        name: "How to monetize apps",
-      },
-    ],
-  },
-  {
-    title: "I’m advanced",
-    subtitle: "Already fluent in tech talk? Give those a try:",
-    image: "/docs/icons/advanced-path.png",
-    links: [
-      {
-        link: "",
-        name: "Messaging Overview",
-      },
-      {
-        link: "",
-        name: "Products SDK",
-      },
-      {
-        link: "",
-        name: "Chat Widget JS API",
-      },
-      {
-        link: "",
-        name: "Webhooks",
-      },
-      {
-        link: "",
-        name: "Reports API",
-      },
-    ],
-  },
-];
-
 export default function Index() {
   const ratingContext = { selectedRating: 0, saveRating: () => {} };
   const versions = getVersionsByGroup(null);
@@ -169,6 +50,7 @@ export default function Index() {
     items: versions,
   };
   const promotionContext = { isActive: false, content: <div /> };
+  const basePath = process.env.CONTEXT === "deploy-preview" ? "" : "/docs";
 
   return (
     <AuthProvider>
@@ -187,7 +69,11 @@ export default function Index() {
               <h2>Build with Platform components</h2>
               <div css={cardsWrapperCss}>
                 {cards.map((card) => (
-                  <Card title={card.title} link={card.link} image={card.image}>
+                  <Card
+                    title={card.title}
+                    link={card.link}
+                    image={basePath + card.image}
+                  >
                     {card.copy}
                   </Card>
                 ))}
@@ -199,7 +85,7 @@ export default function Index() {
                     title={path.title}
                     subtitle={path.subtitle}
                     links={path.links}
-                    image={path.image}
+                    image={basePath + path.image}
                   />
                 ))}
               </div>
