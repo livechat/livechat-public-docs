7b7abebff4699a628153a3048fd72180a129ffd0
Artur Fracala <arturfracala@gmail.com>
2023-03-28 13:33:05 +0200

    remove unused useState

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 /** @jsx jsx */ import { jsx, css } from "@emotion/core";
 import { Input } from "@livechat/design-system";
 
