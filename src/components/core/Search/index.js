0c31b15dad46e8bf0f0d34e5de5ba1d99114fbfc
Wojciech Watroba <w.watroba@livechat.com>
2023-04-11 16:03:58 +0200

    fix hit url

--- src/components/core/Search/index.js
+++ src/components/core/Search/index.js
@@ -1,52 +1,52 @@
 import { DocSearch } from "@docsearch/react";
 import { EXCLUDED_SEARCH_RESULTS, UNCLEAR_SEARCH_MATCHES } from "constant";
 
-const Search = () => {
-  const transformHits = (hits) => {
-    const updatedHits = hits
-      .filter(
-        (hit) => !EXCLUDED_SEARCH_RESULTS.some((slug) => hit.url.includes(slug))
-      )
-      .map((hit) => {
-        let content = null;
-        UNCLEAR_SEARCH_MATCHES.forEach((item) => {
-          const result = hit.url.match(item.regex);
+const baseUrl = process.env.CONTEXT === "deploy-preview" ? "" : "/docs";
 
-          if (Array.isArray(result) && result.length > 0) {
-            content = item.content;
-          }
-        });
+const transformHits = (hits) => {
+  const updatedHits = hits
+    .filter(
+      (hit) => !EXCLUDED_SEARCH_RESULTS.some((slug) => hit.url.includes(slug))
+    )
+    .map((hit) => {
+      let content = null;
+      UNCLEAR_SEARCH_MATCHES.forEach((item) => {
+        const result = hit.url.match(item.regex);
 
-        const url = hit.url.replace(
-          "https://developers.livechat.com",
-          window.location.origin
-        );
-
-        if (content)
-          return {
-            ...hit,
-            content: content,
-            url,
-          };
-
-        return {
-          ...hit,
-          url,
-        };
+        if (Array.isArray(result) && result.length > 0) {
+          content = item.content;
+        }
       });
 
-    return updatedHits;
-  };
+      const url = hit.url.replace(
+        "https://developers.livechat.com/docs",
+        baseUrl
+      );
 
-  return (
-    <DocSearch
-      apiKey={process.env.NEXT_PUBLIC_ALGOLIA_API_KEY}
-      appId={process.env.NEXT_PUBLIC_ALGOLIA_APP_ID}
-      indexName={"livechatinc"}
-      searchParameters={{ hitsPerPage: 7 }}
-      transformItems={transformHits}
-    />
-  );
+      if (content)
+        return {
+          ...hit,
+          content: content,
+          url,
+        };
+
+      return {
+        ...hit,
+        url,
+      };
+    });
+
+  return updatedHits;
 };
+
+const Search = () => (
+  <DocSearch
+    apiKey={process.env.NEXT_PUBLIC_ALGOLIA_API_KEY}
+    appId={process.env.NEXT_PUBLIC_ALGOLIA_APP_ID}
+    indexName={"livechatinc"}
+    searchParameters={{ hitsPerPage: 7 }}
+    transformItems={transformHits}
+  />
+);
 
 export default Search;
