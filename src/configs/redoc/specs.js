de58617d5d9fd3822765e4f2ab5a2f7c7f769bdb
oliwiapolec <54406259+oliwiapolec@users.noreply.github.com>
2025-10-10 07:27:39 +0200

    feat(docs): remove team chat api (#1595)

--- src/configs/redoc/specs.js
+++ src/configs/redoc/specs.js
@@ -2,15 +2,12 @@ const globalAccountsApiSpec = require("./global-accounts-api/spec.yml");
 const customerAccountsApiSpec = require("./customer-accounts-api/spec.yml");
 const customerAccountsApiSpecv2 = require("./customer-accounts-api/specv2.yml");
 const textApiSpec = require("./text-api/spec.yml");
-const chatApiSpec = require("./chat-api/spec.yml");
-
 
 const specs = {
   "global-accounts-api": globalAccountsApiSpec,
   "customer-accounts-api": customerAccountsApiSpec,
   "customer-accounts-api-v2": customerAccountsApiSpecv2,
   "text-api": textApiSpec,
-  "chat-api": chatApiSpec
 };
 
 module.exports = specs;
